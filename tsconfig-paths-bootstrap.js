c2f04161e9d536f2839fdf7cc0f0ebd92f29de4a
rafael <rafaelbordoni00@gmail.com>
2022-11-29 16:35:20 -0300

    add tsconfig bootstrap

---
+++ tsconfig-paths-bootstrap.js
@@ -0,0 +1,10 @@
+const tsConfig = require("./tsconfig.json");
+const tsConfigPaths = require("tsconfig-paths");
+
+const baseUrl = "./";
+const cleanup = tsConfigPaths.register({
+  baseUrl,
+  paths: tsConfig.compilerOptions.paths,
+});
+
+cleanup();
